1364e9028626a103dedf4661a1408eee493b307c
Dell <Dell@192.168.2.239>
2017-12-31 22:56:28 -0500

    initial commit

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,21 @@
+
+
+/*
+ * Print first 20 Fibonacci numbers and their average
+ */
+package fibonacci;
+
+/**
+ *
+ * @author Japreet
+ */
+public class Fibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
